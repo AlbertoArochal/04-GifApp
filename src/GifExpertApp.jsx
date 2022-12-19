699462d94753189f834e33a9e4693678992874ec
AlbertoArochal <arochaldev@gmail.com>
2022-12-19 22:34:04 +0100

    initial commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+export const GifExpertApp = () => {
+  return (
+    <>
+      {" "}
+      {/* TITULO */}
+      <h2>GifExpertApp</h2>
+      {/* INPUT */}
+      {/* LISTADO DE GIFS */}
+      {/*GIF ITEM*/}
+    </>
+  );
+};
