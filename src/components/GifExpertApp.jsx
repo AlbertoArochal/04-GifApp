4f23fa4372906e4b35eaf649d290be0ca28ea50f
AlbertoArochal <arochaldev@gmail.com>
2022-12-24 17:42:24 +0100

    ne

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
-import { GifGrid, GifItem } from "./";
-import { AddCategoryInput } from "./AddCategoryInput.jsx";
+import { GifGrid, GifItem } from "./index";
+import { AddCategoryInput } from "./addCategoryInput";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One Punch"]);
+    const [categories, setCategories] = useState(["One Punch"]);
 
-  const onAddCategory = (newCategory) => {
-    if (categories.includes(newCategory)) return;
-    setCategories([newCategory, ...categories]);
-  };
-  return (
-    <>
-      {" "}
-      {/* TITULO */}
-      <h2>GifExpertApp</h2>
-      {/* INPUT */}
-      {/*<AddCategoryInput setCategories={setCategories}> </AddCategoryInput>*/}
-      <AddCategoryInput onNewCategory={(event) => onAddCategory(event)} />
-      {/* LISTADO DE GIFS */}
-      {categories.map((category, index) => {
-        return <GifGrid category={category} key={index} />;
-      })}
-      {/*GIF ITEM*/}
-    </>
-  );
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        setCategories([newCategory, ...categories]);
+    };
+    return (
+        <>
+            {" "}
+            {/* TITULO */}
+            <h2>GifExpertApp</h2>
+            {/* INPUT */}
+            {/*<AddCategoryInput setCategories={setCategories}> </AddCategoryInput>*/}
+            <AddCategoryInput onNewCategory={(event) => onAddCategory(event)} />
+            {/* LISTADO DE GIFS */}
+            {categories.map((category, index) => {
+                return <GifGrid category={category} key={index} />;
+            })}
+            {/*GIF ITEM*/}
+        </>
+    );
 };
