30d891406765b30cbdff69aa3bdf9713c90503f5
AlbertoArochal <arochaldev@gmail.com>
2022-12-24 17:15:50 +0100

    again

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { GifGrid, GifItem } from "./";
+import { GifGrid, GifItem } from "./index.js";
 import { AddCategoryInput } from "./AddCategoryInput";
 
 export const GifExpertApp = () => {
